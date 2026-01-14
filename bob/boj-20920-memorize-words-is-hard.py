9b0a02b142935aead4cc5fed90101fcf6ec17190
0tak2 <0tak2.code@gmail.com>
2026-01-14 14:27:25 +0900

    [Bob] 2026-01-13 Assignment

---
+++ bob/boj-20920-memorize-words-is-hard.py
@@ -0,0 +1,46 @@
+'''
+- 플랫폼: 프로그래머스
+- 문제: [영단어 암기는 괴로워](https://www.acmicpc.net/problem/20920)
+- 유형: 구현
+'''
+
+import sys
+from functools import cmp_to_key
+
+inputFast = sys.stdin.readline
+printFast = sys.stdout.write
+
+def sort(wordsCount):
+  def comp(a, b):
+    if wordsCount[a] > wordsCount[b]:
+      return -1
+    elif wordsCount[a] < wordsCount[b]:
+      return 1
+    
+    if len(a) > len(b):
+      return -1
+    elif len(a) < len(b):
+      return 1
+    
+    if a < b:
+      return -1
+    elif a > b:
+      return 1
+    
+    return 0
+
+  result = list(wordsCount.keys())
+  result.sort(key=cmp_to_key(comp))
+  return result
+
+n, m = map(int, inputFast().split())
+wordsCount = {}
+for _ in range(n):
+  word = inputFast().rstrip()
+  if len(word) < m:
+    continue
+  
+  wordsCount[word] = wordsCount.get(word, 0) + 1
+
+ans = sort(wordsCount)
+printFast('\n'.join(ans))
